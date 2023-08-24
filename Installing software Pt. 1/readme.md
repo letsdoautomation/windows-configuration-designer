481f24945490392dc0cca6c95d83d5773c6a3a6f
letsdoautomation <me@letsdoautomation.com>
2023-08-24 20:24:40 +0300

    #

--- Installing software Pt. 1/readme.md
+++ Installing software Pt. 1/readme.md
@@ -1,20 +1,21 @@
 ## Software list
 Download: [Telegram](https://desktop.telegram.org/) <br />
-```powershell
-# Make sure that executable name is correct
-cmd /c xcopy /-I tsetup-x64.4.9.2.exe %ALLUSERSPROFILE%\deployment_files\telegram\tsetup.exe && cmd /c reg add "HKLM\SOFTWARE\Microsoft\Active Setup\Installed Components\Install Telegram" /v StubPath /t REG_SZ /d "%ALLUSERSPROFILE%\deployment_files\telegram\tsetup.exe /VERYSILENT /NORESTART"
-```
+Download: [WinRAR](https://www.win-rar.com/download.html?&L=0) <br />
 Download: [Microsoft Teams](https://learn.microsoft.com/en-us/microsoftteams/msi-deployment) <br />
 ```powershell
 msiexec.exe /i Teams_windows_x64.msi /quiet /qn /norestart
 ```
-Download link: [WinRAR](https://www.win-rar.com/download.html?&L=0) <br />
-```powershell
-# Make sure that executable name is correct
-cmd /c copy winrar-x64-623.exe %TEMP% && cmd /c %TEMP%\winrar-x64-623.exe /S
-```
+
 Disable "Privacy Experience" <br />
 <img src="img/privacySettings.png" width=40% height=40%>
 ```powershell
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\OOBE /v DisablePrivacyExperience /t REG_DWORD /d 1
+```
+
+## PSEXEC
+[PSEXEC with Windows Configuration Designer example](https://learn.microsoft.com/en-us/windows/configuration/provisioning-packages/provisioning-script-to-install-app#powershell-example) <br />
+[PSEXEC download](https://learn.microsoft.com/en-us/sysinternals/downloads/psexec)
+
+```powershell
+powershell.exe -ExecutionPolicy Bypass -File run.ps1
 ```
---
+++ Installing software Pt. 1/run.ps1
@@ -0,0 +1,46 @@
+# WinRAR Deployment
+# Make sure that executable name is correct
+$winrar_exe = 'winrar-x64-623.exe'
+$winrar_silent_args = '/S'
+
+$winrar_path = gi $winrar_exe | select -expand FullName
+
+$install_params = @{
+    FilePath     = 'PsExec.exe'
+    ArgumentList = "-accepteula -i -s $($winrar_path) $($winrar_silent_args)"
+    NoNewWindow  = $true
+    Wait         = $true
+}
+
+Start-Process @install_params
+
+New-Item "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\WinRAR run once" -Force
+
+$reg_active_setup_run_winrar = @{
+    Path  = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\WinRAR run once"
+    Name  = "StubPath"
+    Value = 'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v RunWinRAR /t REG_SZ /d "{0}\WinRAR\WinRAR.exe"' -f $env:ProgramFiles
+    Force = $true
+}
+
+New-ItemProperty @reg_active_setup_run_winrar
+
+# Telegram Deployment
+# Make sure that executable name is correct
+$telegram_exe = 'tsetup-x64.4.9.2.exe'
+$telegram_args = '/VERYSILENT /NORESTART'
+
+New-Item "$($env:ProgramData)\deployment_files\telegram" -ItemType Directory -Force
+
+cp $telegram_exe "$($env:ProgramData)\deployment_files\telegram\tsetup.exe" -Force
+
+New-Item "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\Install telegram" -Force
+
+$reg_active_setup_run_winrar = @{
+    Path  = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\Install telegram"
+    Name  = "StubPath"
+    Value = 'reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce" /v InstallTelegram /t REG_SZ /d "{0}\deployment_files\telegram\tsetup.exe {1}"' -f $env:ProgramData, $telegram_args
+    Force = $true
+}
+
+New-ItemProperty @reg_active_setup_run_winrar
